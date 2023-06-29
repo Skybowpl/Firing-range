26156c0dc1d4194a63098043dcf41697dedb36a4
Skybowpl <szymus456@gmail.com>
2023-06-29 20:53:32 +0200

    Asdding reset to exercise after fer second

--- Firing range/Assets/Scripts/FlickExercise.cs
+++ Firing range/Assets/Scripts/FlickExercise.cs
@@ -1,21 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class FlickExercise : MonoBehaviour
 {
     [SerializeField] private GameObject hitTarget;
     [SerializeField] private float minSpred = -2;
-    [SerializeField] private float maxSpred = 2 ;
+    [SerializeField] private float maxSpred = 2;
+    [SerializeField] private float exerciseTime = 5;
+    private float timerEnd;
     public void spawnTarget()
     {
-        GameObject target = Instantiate(hitTarget, gameObject.transform.position + new Vector3(0, Random.Range(minSpred, maxSpred), Random.Range(minSpred, maxSpred)), Quaternion.identity);
-        target.transform.parent = gameObject.transform;
+        if (Time.time < timerEnd)
+        {
+            GameObject target = Instantiate(hitTarget, gameObject.transform.position + new Vector3(0, Random.Range(minSpred, maxSpred), Random.Range(minSpred, maxSpred)), Quaternion.identity);
+            target.transform.parent = gameObject.transform;
+        }
+        else
+        {
+            StartChange(true);
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        gameObject.GetComponent<Collider>().enabled = false;
-        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        timerEnd = Time.time + exerciseTime;
+        StartChange(false);
+        StartCoroutine(StopExercise(exerciseTime));
         spawnTarget();
+    }
+
+    private void StartChange(bool exerciseState)
+    {
+        gameObject.GetComponent<Collider>().enabled = exerciseState;
+        gameObject.GetComponent<MeshRenderer>().enabled = exerciseState;
+    }
+
+    private IEnumerator StopExercise(float time)
+    {
+        yield return new WaitForSeconds(time);
+        StartChange(true);
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
     }
 }
